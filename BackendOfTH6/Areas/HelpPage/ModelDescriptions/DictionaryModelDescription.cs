74e2a192dcb27f7a9093b12be2809462b16f7855
hoanghieu1109 <hoang2uanghieu@gmail.com>
2020-10-09 19:10:06 +0700

    first commit

---
+++ BackendOfTH6/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BackendOfTH6.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ BackendOfTH6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BackendOfTH6.WebApiApplication" Language="C#" %>
